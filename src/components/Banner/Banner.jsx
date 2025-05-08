cde33683c08520a05de5fb924f11104973f221a1
Pedro Henrique Cunha Moda <phcunha47@gmail.com>
2025-05-08 20:25:46 -0300

    fix: corrigindo import da imagem do banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import styles from './Banner.module.css'
 function Banner() {
   return (
     <div className={styles.container}>
-      <img src="/src/assets/drinks.jpg" alt="Drinks" />
+      <img src="/drinks.jpg" alt="Drinks" />
       <p className={styles.text}>Your next favorite drink awaits!</p>
     </div>
   )
